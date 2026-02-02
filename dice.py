a4c29f0af2305d97353ae7f83c0694fbfcd52a20
xfull322 <evan.perotin.pro@gmail.com>
2026-02-02 14:23:47 +0100

    class créé et dice importer

---
+++ dice.py
@@ -0,0 +1,26 @@
+import random
+
+
+class Dice:
+    def __init__(self, f=6, c="red", m="plastic"):
+        self.faces = f
+        self.color = c
+        self.material = m
+
+    def __str__(self):
+        return f"This a {self.color} {self.material} with {self.faces} faces"
+
+    def __eq__(self, another_dice):
+        return self.faces == another_dice.faces and self.color == another_dice.color and self.material == another_dice.material
+
+    def roll(self):
+        """This method should return a random number..."""
+        return random.randint(1, self.faces)
+
+
+class RiggedDice(Dice):
+    def roll(self, rigged=False):
+        if not rigged:
+            return super().roll()
+        else:
+            return self.faces
